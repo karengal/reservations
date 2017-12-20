e0dfc9d15764c56deed3c854fcb2f0e08a581dd3
Karen Gal <karengal@gmail.com>
2017-12-20 19:05:40 +0200

    added basic without char check

--- main.js
+++ main.js
@@ -3,10 +3,25 @@ var reservations = {
   'Ted': { claimed: true }
 }
 
-var name = prompt('Please enter the name for your reservation');
-
+//this is not ignoring case yet
 var claimReservation = function () {
-  // write your code here!
+  var inputName = prompt('Please enter the name for your reservation');
+  var inputLower = inputName.toLowerCase();
+  if(!reservations[inputName])
+    //if(!reservations[inputLower] && inputName.toLowerCase() != inputLower)
+      addReservation(inputName);
+  else{
+      if (reservations[inputName].claimed)
+        alert("reservation already exists under your name");
+      else 
+        alert("Welcome");    
+  } 
 }
-
 claimReservation();
+
+ var addReservation = function(nameToAdd) {
+  reservations[nameToAdd] = {claimed : true};
+  alert("Hello! A reservation was made for you");
+ }
+
+
