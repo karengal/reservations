7f285f53cc894fd1274611224facf1423e19ce82
Karen Gal <karengal@gmail.com>
2017-12-21 23:21:56 +0200

    added functions working correctly

--- main.js
+++ main.js
@@ -3,25 +3,43 @@ var reservations = {
   'Ted': { claimed: true }
 }
 
-//this is not ignoring case yet
-var claimReservation = function () {
-  var inputName = prompt('Please enter the name for your reservation');
-  var inputLower = inputName.toLowerCase();
-  if(!reservations[inputName])
-    //if(!reservations[inputLower] && inputName.toLowerCase() != inputLower)
-      addReservation(inputName);
-  else{
-      if (reservations[inputName].claimed)
-        alert("reservation already exists under your name");
-      else 
-        alert("Welcome");    
-  } 
-}
-claimReservation();
+var claimReservation = function (inputName) {
+  //var inputName = prompt('Please enter the name for your reservation');
+
+  var person = findPerson(inputName);
+  if (person){
+    
+    if (person.claimed)
+      alert("reservation already exists under your name");
+    else
+      alert("Welcome");
+  }
+
+  else
+    addReservation(inputName);
+  }
 
  var addReservation = function(nameToAdd) {
   reservations[nameToAdd] = {claimed : true};
   alert("Hello! A reservation was made for you");
  }
 
+ var findPerson = function(nameToFind) {
+   //add checking lower case
+  if(reservations[nameToFind])
+    return reservations[nameToFind];
+  else
+    for (var key in reservations){
+      if (key.toLowerCase() === nameToFind)
+        return reservations[key];
+    }
+    
+    return null;
+}
+
+claimReservation("Ted");
+claimReservation("Bob");
+claimReservation("ted");
+claimReservation("Karen");
+claimReservation("bob");
 
